882e0e11ea82e59db083b7dc420e88058048777d
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-08-12 21:42:12 +0200

    Start creating header component

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+  background-color: #393939;
+  width: 320px;
+  height: 100px;
+  border-radius: 16px;
+  display: flex;
+  justify-content: center;
+  font-size: 40px;
+  color: white;
+  //drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
+  p {
+    margin: 0;
+    align-self: center;
+  }
+`
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.styles'
+import {HeaderStyled} from "./Header.styles";
+
+export const Header = () => {
+    return (
+    <HeaderStyled>
+        <p>Tic - Tac -Toe</p>
+    </HeaderStyled>
+    )}
