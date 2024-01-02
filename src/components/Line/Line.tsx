813e202a27ea6b806a708e627116cac3216c7b03
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-01-02 11:50:14 +0100

    refactor code

--- src/components/Line/Line.tsx
+++ src/components/Line/Line.tsx
@@ -1,14 +1,11 @@
-import { getStyleOfLine } from '../Middle/Middle.helpers'
 import { LineStyled } from './Line.styles'
 import { LineTypes } from './Line.types'
 
-export const Line = ({ transform, width }: LineTypes) => {
-  return (
-    <LineStyled
-      style={{
-        transform: transform,
-        width: width,
-      }}
-    />
-  )
-}
+export const Line = ({ transform, width }: LineTypes) => (
+  <LineStyled
+    style={{
+      transform: transform,
+      width: width,
+    }}
+  />
+)
