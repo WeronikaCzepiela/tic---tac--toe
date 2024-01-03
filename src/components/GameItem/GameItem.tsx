636252e8753dd8d7e0658f8e971f843c38ade145
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-01-03 13:13:02 +0100

    create style for game item for desktop app

--- src/components/GameItem/GameItem.tsx
+++ src/components/GameItem/GameItem.tsx
@@ -2,16 +2,45 @@ import { GameItemStyled } from './GameItem.styles'
 import { GameItemType, GameItemTypes } from './GameItem.types'
 import XIcon from '../../assets/image/x.svg'
 import OIcon from '../../assets/image/o.svg'
+import { DIMENSION, useWindowDimensions } from '../../utils'
 
 export const GameItem = ({ id, type, onClick }: GameItemTypes) => {
+  const { width } = useWindowDimensions()
+
+  const getCorrectWidthOfIcon = () => {
+    if (width > DIMENSION) return 80
+
+    return 60
+  }
+
+  const getCorrectHeightOfIcon = () => {
+    if (width > DIMENSION) return 76
+
+    return 56
+  }
+
   const icons = [
     {
       type: GameItemType.CROSS,
-      image: <img src={XIcon} color={'#1C6ECF'} height={56} width={60} />,
+      image: (
+        <img
+          src={XIcon}
+          color={'#1C6ECF'}
+          height={getCorrectHeightOfIcon()}
+          width={getCorrectWidthOfIcon()}
+        />
+      ),
     },
     {
       type: GameItemType.CIRCLE,
-      image: <img src={OIcon} color={'#1C6ECF'} height={56} width={60} />,
+      image: (
+        <img
+          src={OIcon}
+          color={'#1C6ECF'}
+          height={getCorrectHeightOfIcon()}
+          width={getCorrectWidthOfIcon()}
+        />
+      ),
     },
   ]
 
